ac85a474449b43f00cf03128e76d159c7f8815ca
a.odegov <a.odegov@oos.ru>
2025-04-18 20:03:59 +0300

    add params

--- lib/Src/Migration/UserField/Providers/EnumerationProvider.php
+++ lib/Src/Migration/UserField/Providers/EnumerationProvider.php
@@ -15,6 +15,9 @@ class EnumerationProvider extends UserFieldProvider
         return 'enumeration';
     }
 
+    /**
+     * @return EnumValue
+     */
     public function createEnumValue(): EnumValue
     {
         $enumValue = new EnumValue();
@@ -22,6 +25,53 @@ class EnumerationProvider extends UserFieldProvider
         return $enumValue;
     }
 
+    /**
+     * @param int $height
+     * @return $this
+     */
+    public function setListHeight(int $height): self
+    {
+        $this->settings['LIST_HEIGHT'] = max(1, $height);
+        return $this;
+    }
+
+    /**
+     * @param string $display
+     * @return $this
+     */
+    public function setDisplay(string $display): self
+    {
+        $allowedDisplays = ['LIST', 'CHECKBOX', 'UI', 'DIALOG'];
+        $this->settings['DISPLAY'] = in_array($display, $allowedDisplays) ? $display : 'LIST';
+        return $this;
+    }
+
+    /**
+     * @param string $caption
+     * @return $this
+     */
+    public function setCaptionNoValue(string $caption): self
+    {
+        $this->settings['CAPTION_NO_VALUE'] = $caption;
+        return $this;
+    }
+
+    /**
+     * @param bool $showNoValue
+     * @return $this
+     */
+    public function setShowNoValue(bool $showNoValue): self
+    {
+        $this->settings['SHOW_NO_VALUE'] = $showNoValue ? 'Y' : 'N';
+        return $this;
+    }
+
+    /**
+     * @param int $fieldId
+     * @param array $field
+     * @param string $moduleId
+     * @return void
+     */
     public function afterAdd(int $fieldId, array $field, string $moduleId): void
     {
         if (empty($this->enumValues)) {
@@ -32,8 +82,20 @@ class EnumerationProvider extends UserFieldProvider
         $enumData = [];
         $count = 0;
         foreach ($this->enumValues as $enumValue) {
-            $enumData['n'.$count++] = $enumValue->getElementToArray();
+            $enumData['n' . $count++] = $enumValue->getElementToArray();
         }
         $enum->SetEnumValues($fieldId, $enumData);
+    }
+
+    /**
+     * Возвращает параметры провайдера в виде массива.
+     *
+     * @return array
+     */
+    public function getParamsToArray(): array
+    {
+        return array_merge(parent::getParamsToArray(), [
+            'SETTINGS' => $this->settings,
+        ]);
     }
 }
